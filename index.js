62ea217dfa5885c5518d391bf381f01ab34f7c26
skn3 <jon@skn3.com>
2015-11-05 12:35:51 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+function Destinations(where, options, result) {
+	//what type of reroute do we need?
+	var type = typeof where;
+	switch (type) {
+		case 'function':
+			//single func call
+			result.push(function (req, res, next) {
+				return where(req, res, next, options);
+			});
+			break;
+
+		case 'object':
+			//route based on request method
+			result.push(function (req, res, next) {
+				if (typeof where[req.method] != 'function') {
+					throw(Error('WhereTo unknown request method ' + req.method));
+				}
+				return where[req.method](req, res, next, options);
+			});
+			break;
+
+		case 'array':
+			//recurse array
+			for (var item of where) {
+				Destinations(item, options, result);
+			}
+			break;
+		default:
+			throw(Error('WhereTo invalid type ' + type));
+	}
+
+	//chain
+	return result;
+}
+
+module.exports = function (where, options) {
+	//get routes
+	var result = [];
+	Destinations(where, options, result);
+
+	//check for error
+	if (result.length == 0) {
+		throw(Error('WhereTo no routes specified'));
+	}
+
+	//do return
+	if (result.length == 1) {
+		//return 1 item
+		return result[0];
+	} else {
+		//return the array of
+		return result;
+	}
+}
